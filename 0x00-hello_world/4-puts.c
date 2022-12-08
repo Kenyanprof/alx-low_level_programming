1a7262215f7bdcc6ff6f76173bb7c8597c0b4047
Kenyanprof <odhiamboandrew76@gmail.com>
2022-12-08 04:48:48 -0800

    Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /*
  * main - print a line of code using puts
  *
@@ -9,3 +10,4 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+

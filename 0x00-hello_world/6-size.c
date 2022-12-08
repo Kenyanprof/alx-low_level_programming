04a54d4fea7b04115eb8ac2665d5399430921e52
Kenyanprof <odhiamboandrew76@gmail.com>
2022-12-08 06:32:20 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print the sizes of data types in C.
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int i =%d;
+	int char =%c;
+	int float =%f
+	int double =%if
+	printf("size of int i %d: %lu bytes,\n");
+	printf("size of char %c: %lu bytes,\n");
+	print("size of float %f: %lu bytes,\n");
+	printf("size of double %if: %lu bytes,\n");
+	return (0);
+}

9c649b0c817a59b606b7e466422d4b9ad5962600
Kenyanprof <odhiamboandrew76@gmail.com>
2022-12-18 01:00:20 -0800

    large numbers

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * main - print 3 numbers the largest 
+ *@a: first integer
+ *@b: second integer
+ *@c: third integer
+ *
+ * Return: o
+ */
+int largest_number(int a, int b, int c)
+{
+	int largest 
+
+		if (a > b && a > c)
+		{
+			largest = a;
+		}
+		else if (b > c)
+		{
+			largest = b;
+		}
+		else 
+		{
+			largest = c;
+		}
+	return (largest);
+}
+
